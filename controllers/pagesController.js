45163b6af70dcac5c1d770c87fa0559a06d0fb2c
AllieAdams <200380386@student.georgianc.on.ca>
2019-05-26 13:55:45 -0400

    added app.js fix

--- controllers/pagesController.js
+++ controllers/pagesController.js
@@ -2,5 +2,5 @@ exports.show = (request, response) => {
     const path = (request.path === '/') ? '/home' : request.path;
 
     //render the view
-    response.render(`pages${path; //views/pages/about
-}
+    response.render(`pages${path}`); //views/pages/about
+};
